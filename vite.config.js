5b0b630f6254208e51efc49991047ca0338eea8e
Christopher Greaves <chgreaves123@gmail.com>
2025-12-18 00:04:18 -0500

    fixed yaml

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    allowedHosts: [
+      'restaurant-frontend-7jm9.onrender.com',
+      '.onrender.com' // To allow all Render subdomains
+    ]
+  }
 })
